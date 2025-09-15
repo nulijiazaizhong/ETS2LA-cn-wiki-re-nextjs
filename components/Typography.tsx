eaf5f09709ada74c277e2f17feb2c21eb2c1b503
nulijiazaizhong <127476147+nulijiazaizhong@users.noreply.github.com>
2025-09-15 14:31:03 +0800

    增加标题上方与下方的间距

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -5,10 +5,10 @@ import { cn } from '@/lib/utils'
 const typographyVariants = cva('text-foreground', {
   variants: {
     variant: {
-      h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
-      h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
-      h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
-      h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
+      h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4 mt-6',
+      h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-4 mt-6',
+      h3: 'scroll-m-20 text-2xl font-semibold tracking-tight mb-4 mt-6',
+      h4: 'scroll-m-20 text-xl font-semibold tracking-tight mb-4 mt-6',
       h5: 'scroll-m-20 text-lg font-semibold tracking-tight',
       p: 'leading-7 [&:not(:first-child)]:mt-6',
       blockquote: 'mt-6 border-l-2 pl-6 italic',
