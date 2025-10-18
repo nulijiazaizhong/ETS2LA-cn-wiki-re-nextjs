9b467990fdfaf0440654f779c27693a812268586
BGYdook <3317732779@qq.com>
2025-10-18 08:29:42 +0800

    更新了阅读流畅性

--- app/docs/base/install/page.tsx
+++ app/docs/base/install/page.tsx
@@ -141,7 +141,7 @@ export default function InstallPage() {
       <div className="not-prose my-4 rounded-lg border border-l-4 border-green-500 bg-green-500/10 p-4 text-green-700 dark:text-green-300">
         <p className="font-bold">提示</p>
         <p>
-          正在安装用时不一，具体用时根据您的电脑配置和网络环境而决定，电脑配置好、网络畅通的可能5分钟不到就完成了，电脑配置不高、网络不通畅的可能需要30分钟或以上
+          安装时间取决于您的电脑配置和网络环境。在配置良好、网络畅通的情况下，通常可在5分钟内完成；若配置较低或网络不佳，则可能延长至30分钟或更久。
         </p>
       </div>
 
